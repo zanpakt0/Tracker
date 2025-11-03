1f650479c8b3b9206a6d5a3db8e3de386a58d114
Konstantin Zhukov <k.zhukov@vk.team>
2025-11-03 10:43:00 +0300

    Initial Commit

---
+++ TrackerTests/TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerTests.swift
+//  TrackerTests
+//
+//  Created by Zhukov Konstantin on 03.11.2025.
+//
+
+import Testing
+@testable import Tracker
+
+struct TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
